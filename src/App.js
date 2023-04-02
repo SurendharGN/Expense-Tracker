7e9daedf58e0a06fbb8e3a29a96daa5c79b1f189
Surendhar <studyspace.ie@gmail.com>
2023-04-02 12:05:31 +0530

    Revenue card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import Income from './components/Income';
+import Revenue from './components/Revenue';
+import Tracker from './components/Tracker';
 import './App.css';
 
 function App() {
     
   return (
-    <div className="App">
-      <h1 className="">Expense tracker</h1>
-      
+    <div className="grid grid-cols-2 grid-rows-2 h-screen text-center">
+
+        <div id="tracker" className="bg-white col-span-2">
+            <Tracker/>
+        </div>
+        <div id="income" className="bg-zinc-100">
+            <Income/>
+        </div>
+
+        <div id="expense" className="bg-zinc-100 border-l border-l-black">
+            <Revenue/>
+        </div>
+
+        
+
     </div>
   );
 }
---
+++ src/components/Income.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Cart, CardHeader, CartContent, Typography} from "@mui/material"
+const Income = () => {
+  return (
+    <div>Income</div>
+  )
+}
+
+export default Income
